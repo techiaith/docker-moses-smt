9b63fe12d1447636ecea335e578ba7f7eeba9240
Marko Zabreznik <marko@zabreznik.net>
2015-06-06 21:26:33 +0200

    making mtdk-01-prepare-corpus respect source and target language settings

--- mtdk/mtdk-01-prepare-corpus.sh
+++ mtdk/mtdk-01-prepare-corpus.sh
@@ -2,9 +2,11 @@
 
 usage() { echo "Usage: $0 -m <moses installation home>
 			  -h <path to mosesmodels home> 
-			  -e <model or engine name>" 1>&2; exit 1; }
+			  -e <model or engine name> 
+			  -s <machine translation source language - en or cy>
+			  -t <machine translation target language - en or cy>" 1>&2; exit 1; }
 
-while getopts ":m:h:e:" o; do
+while getopts ":m:h:e:s:t:" o; do
 	case "${o}" in
 		m)	MOSES_HOME=${OPTARG}
 			;;
@@ -14,6 +16,12 @@ while getopts ":m:h:e:" o; do
 		e)
 			NAME=${OPTARG} 
 			;;
+		s)
+			SOURCE_LANG=${OPTARG}		
+			;;		
+		t)
+			TARGET_LANG=${OPTARG}		
+			;;
 		*)
 			usage	
 			;;
@@ -21,7 +29,7 @@ while getopts ":m:h:e:" o; do
 done  
 shift $((OPTIND-1))
 
-if [ -z "${MOSES_HOME}" ] || [ -z "${MOSESMODELS_HOME}" ] || [ -z "${NAME}" ]; then
+if [ -z "${MOSES_HOME}" ] || [ -z "${MOSESMODELS_HOME}" ] || [ -z "${NAME}" ] || [ -z "${SOURCE_LANG}" ] || [ -z "${TARGET_LANG}" ]; then
     usage
 fi
 
@@ -31,15 +39,15 @@ echo "##### PREPARING TRAINING CORPUS #####"
 mkdir -p temp
 
 echo "##### LOWER CASING #####"
-${MOSES_HOME}/mosesdecoder/scripts/tokenizer/lowercase.perl -l cy < "${NAME}.cy" > "temp/${NAME}.lower.cy"
-${MOSES_HOME}/mosesdecoder/scripts/tokenizer/lowercase.perl -l en < "${NAME}.en" > "temp/${NAME}.lower.en"
+${MOSES_HOME}/mosesdecoder/scripts/tokenizer/lowercase.perl -l ${TARGET_LANG} < "${NAME}.${TARGET_LANG}" > "temp/${NAME}.lower.${TARGET_LANG}"
+${MOSES_HOME}/mosesdecoder/scripts/tokenizer/lowercase.perl -l ${SOURCE_LANG} < "${NAME}.${SOURCE_LANG}" > "temp/${NAME}.lower.${SOURCE_LANG}"
 
 
 echo "##### TOKENIZATION ######"
-${MOSES_HOME}/mosesdecoder/scripts/tokenizer/tokenizer.perl -l cy < "temp/${NAME}.lower.cy" > "temp/${NAME}.tok.cy" 
-${MOSES_HOME}/mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < "temp/${NAME}.lower.en" > "temp/${NAME}.tok.en" 
+${MOSES_HOME}/mosesdecoder/scripts/tokenizer/tokenizer.perl -l ${TARGET_LANG} < "temp/${NAME}.lower.${TARGET_LANG}" > "temp/${NAME}.tok.${TARGET_LANG}" 
+${MOSES_HOME}/mosesdecoder/scripts/tokenizer/tokenizer.perl -l ${SOURCE_LANG} < "temp/${NAME}.lower.${SOURCE_LANG}" > "temp/${NAME}.tok.${SOURCE_LANG}" 
 
 #echo "##### CLEANING #####"
-${MOSES_HOME}/mosesdecoder/scripts/training/clean-corpus-n.perl "temp/${NAME}.tok" cy en "${NAME}.clean" 1 80
+${MOSES_HOME}/mosesdecoder/scripts/training/clean-corpus-n.perl "temp/${NAME}.tok" ${TARGET_LANG} ${SOURCE_LANG} "${NAME}.clean" 1 80
 
 cd -
