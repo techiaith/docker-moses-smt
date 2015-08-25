2e238c01b252053bed1286e2f6fdc500c1db4688
Uned Technolegau Iaith <techiaith@bangor.ac.uk>
2015-08-25 12:01:13 +0100

    -y apt-get install libxmlrpc

--- get/debian/install.sh
+++ get/debian/install.sh
@@ -3,7 +3,7 @@ set -e # exit on failure
 sudo apt-get update
 sudo apt-get install -y make g++ git mercurial autotools-dev automake libtool zlib1g-dev libbz2-dev libboost-all-dev libxmlrpc-core-c3-dev zip
 # libxmlrpc-c3-dev on Ubuntu 12.04, libxmlrpc-c++8-dev on Ubuntu 14.04
-sudo apt-get install libxmlrpc-c3-dev || sudo apt-get install libxmlrpc-c++8-dev
+sudo apt-get install -y libxmlrpc-c3-dev || sudo apt-get install -y libxmlrpc-c++8-dev
 sudo locale-gen cy_GB.UTF-8 en_US.UTF-8
 sudo dpkg-reconfigure locales
 
