fa133c55a027f565149ff08a207848249bdcf379
Uned Technolegau Iaith <techiaith@bangor.ac.uk>
2016-03-10 17:24:53 +0000

    pecynnau RedHat ychwanegol / additional packages for RedHat

--- get/redhat/install.sh
+++ get/redhat/install.sh
@@ -1,4 +1,6 @@
 set -e # exit on failure
 sudo yum update
-sudo yum group install "Development Tools"
+sudo yum group install "Development Tools" "Additional Development"
 sudo yum install git wget zlib-devel python-devel boost-devel
+sudo yum install xmlrpc-c-devel
+sudo yum install rpmdevtools
